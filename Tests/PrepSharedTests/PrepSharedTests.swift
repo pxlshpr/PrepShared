228c5ecd0b6d708b4dd83315f22f4ec500c7770b
Ahmed Khalaf <pxlshpr@gmail.com>
2023-09-11 08:07:43 +0500

    Initial Commit

---
+++ Tests/PrepSharedTests/PrepSharedTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import PrepShared
+
+final class PrepSharedTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
