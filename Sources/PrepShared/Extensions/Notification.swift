a0edcce9a15685654710662a624dbf631b8cee38
Ahmed Khalaf <pxlshpr@gmail.com>
2023-09-13 19:37:37 +0500

    tag increment

--- Sources/PrepShared/Extensions/Notification.swift
+++ Sources/PrepShared/Extensions/Notification.swift
@@ -4,24 +4,72 @@ public extension Notification.Name {
     static var didUpdateFood: Notification.Name { return .init("didUpdateFood") }
     static var didUpdateWord: Notification.Name { return .init("didUpdateWord") }
     static var networkDidBecomeOnline: Notification.Name { return .init("networkDidBecomeOnline") }
+    
+    /// UI Changes
+    static var safeAreaDidChange: Notification.Name { return .init("safeAreaDidChange") }
+    static var didTapToday: Notification.Name { return .init("didTapToday") }
+    static var didTapAddFood: Notification.Name { return .init("didTapAddFood") }
+    static var didTapMealItem: Notification.Name { return .init("didTapMealItem") }
+    static var scrollItemFormToQuantity: Notification.Name { return .init("scrollItemFormToQuantity") }
+
+    /// Backend Changes
+    static var didPopulate: Notification.Name { return .init("didPopulate") }
+    static var didUpdateDay: Notification.Name { return .init("didUpdateDay") }
+//    static var didUpdateFood: Notification.Name { return .init("didUpdateFood") }
+    static var didUpdateSettings: Notification.Name { return .init("didUpdateSettings") }
+
 }
 
+//public extension Notification {
+//    enum Keys: String {
+//        case food = "food"
+//    }
+//}
+
 public extension Notification {
-    enum Keys: String {
+    enum PrepKeys: String {
+        case day = "day"
+        case date = "date"
+        case meal = "meal"
         case food = "food"
+        case settings = "settings"
+        case foodItem = "foodItem"
+        case safeArea = "safeArea"
+        case foodIDToRemove = "foodIDToRemove"
     }
 }
 
-public func post(_ name: Notification.Name, _ userInfo: [Notification.Keys : Any]? = nil) {
+public func post(_ name: Notification.Name, _ userInfo: [Notification.PrepKeys : Any]? = nil) {
     NotificationCenter.default.post(name: name, object: nil, userInfo: userInfo)
 }
 
 public extension Notification {
-    func value(for key: Notification.Keys) -> Any? {
+    func value(for key: Notification.PrepKeys) -> Any? {
         userInfo?[key]
+    }
+    
+    var foodItem: FoodItem? {
+        value(for: .foodItem) as? FoodItem
+    }
+    var foodIDToRemove: UUID? {
+        value(for: .foodIDToRemove) as? UUID
     }
     
     var food: Food? {
         value(for: .food) as? Food
     }
 }
+
+//public func post(_ name: Notification.Name, _ userInfo: [Notification.Keys : Any]? = nil) {
+//    NotificationCenter.default.post(name: name, object: nil, userInfo: userInfo)
+//}
+//
+//public extension Notification {
+//    func value(for key: Notification.Keys) -> Any? {
+//        userInfo?[key]
+//    }
+//    
+//    var food: Food? {
+//        value(for: .food) as? Food
+//    }
+//}
