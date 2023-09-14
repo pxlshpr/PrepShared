f61a3285bd9290ec58af58ced0423163afba3fbc
Ahmed Khalaf <pxlshpr@gmail.com>
2023-09-14 20:52:35 +0500

    tag increment

---
+++ Sources/PrepShared/Observables/PagedArray.swift
@@ -0,0 +1,126 @@
+import Foundation
+import SwiftUI
+import OSLog
+
+import SwiftSugar
+
+private let logger = Logger(subsystem: "PagedArray", category: "")
+
+@Observable public class PagedArray<Type: Equatable> {
+    public var array: [Type]
+    public var isLoadingPage: Bool
+    public var currentPage: Int
+    public var canLoadMorePages: Bool
+    public var shouldShowEmptyState: Bool
+    
+    public var fetchPageHandler: (Int, String?) async throws -> [Type]
+    public var didFetchPageHandler: (() -> ())?
+
+    public var searchTask: Task<Void, Error>? = nil
+    public var displayedSearchText: String? = nil
+
+    public init(
+        _ fetchPageHandler: @escaping (Int, String?) async throws -> [Type],
+        _ didFetchPageHandler: (() -> ())? = nil
+    ) {
+        self.array = []
+        self.isLoadingPage = true
+        self.shouldShowEmptyState = true
+        self.currentPage = 1
+        self.canLoadMorePages = true
+        self.fetchPageHandler = fetchPageHandler
+        self.didFetchPageHandler = didFetchPageHandler
+    }
+    
+    //MARK: Public Functions
+    public func performSearch(_ text: String, showingLoading: Bool = true) {
+        searchTask?.cancel()
+        
+        let text = text.trimmingWhitespaces.lowercased()
+        guard !text.isEmpty else {
+            clear()
+            fetchFirstPage(showingLoading: showingLoading)
+            return
+        }
+
+        shouldShowEmptyState = false
+        isLoadingPage = showingLoading
+        
+        searchTask = Task {
+            
+            /// Wait a bit to allow fast typing to proceed uninterrupted
+            try await sleepTask(0.3, tolerance: 0.05)
+            try Task.checkCancellation()
+
+            await MainActor.run {
+                displayedSearchText = text
+            }
+            try Task.checkCancellation()
+
+            try await fetchFirstPage(searchText: text, showingLoading: showingLoading)
+        }
+    }
+
+    public func fetchFirstPage(showingLoading: Bool = true) {
+        Task {
+            try await fetchFirstPage(searchText: nil, showingLoading: showingLoading)
+        }
+    }
+    
+    public func fetchFirstPage(searchText: String? = nil, showingLoading: Bool = true) async throws {
+        await MainActor.run {
+            currentPage = 1
+            shouldShowEmptyState = false
+            isLoadingPage = showingLoading
+        }
+        try await fetchNextPage(searchText: searchText)
+    }
+    
+    public func fetchNextPageIfAvailable(for element: Type, searchText: String? = nil) {
+        if shouldLoadNextPage(for: element), !isLoadingPage, canLoadMorePages {
+            isLoadingPage = true
+            Task.detached(priority: .userInitiated) {
+                try await self.fetchNextPage(searchText: searchText)
+            }
+        }
+    }
+
+    func fetchNextPage(searchText: String? = nil) async throws {
+        let results = try await fetchPageHandler(currentPage, searchText)
+        try Task.checkCancellation()
+        await MainActor.run {
+            withAnimation(.smooth) {
+                append(results)
+            }
+            didFetchPageHandler?()
+        }
+    }
+    
+    //MARK: Helpers
+    
+    func clear() {
+        array = []
+        isLoadingPage = true
+        shouldShowEmptyState = true
+        currentPage = 1
+        canLoadMorePages = true
+    }
+    
+    func append(_ results: [Type]) {
+        if currentPage == 1 {
+            array = results
+        } else {
+            array.append(contentsOf: results)
+        }
+            
+        canLoadMorePages = results.count == FoodsPageSize
+        currentPage += 1
+        isLoadingPage = false
+        shouldShowEmptyState = array.isEmpty
+    }
+    
+    func shouldLoadNextPage(for element: Type) -> Bool {
+        let thresholdIndex = array.index(array.endIndex, offsetBy: -5)
+        return array.firstIndex(where: { $0 == element }) == thresholdIndex
+    }
+}
