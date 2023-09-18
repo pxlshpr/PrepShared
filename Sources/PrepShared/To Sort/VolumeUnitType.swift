94114f59fca2601a1173744ff8ae1a86127ec98a
Ahmed Khalaf <pxlshpr@gmail.com>
2023-09-18 23:36:24 +0500

    tag increment

--- Sources/PrepShared/To Sort/VolumeUnitType.swift
+++ Sources/PrepShared/To Sort/VolumeUnitType.swift
@@ -74,3 +74,65 @@ public extension VolumeUnitType {
         }
     }
 }
+
+public extension VolumeUnitType {
+    var units: [VolumeUnit] {
+        switch self {
+        case .cup:
+            return [
+                .cupMetric,
+                .cupUSLegal,
+                .cupUSCustomary,
+                .cupImperial,
+                .cupCanada,
+                .cupLatinAmerica,
+                .cupJapanTraditional,
+                .cupJapanModern,
+                .cupRussianProper,
+                .cupRussianGlassRegular,
+                .cupRussianGlassLarge,
+            ]
+        case .teaspoon:
+            return [
+                .teaspoonMetric,
+                .teaspoonUS
+            ]
+        case .tablespoon:
+            return [
+                .tablespoonMetric,
+                .tablespoonUS,
+                .tablespoonAustralia
+            ]
+        case .fluidOunce:
+            return [
+                .fluidOunceUSNutritionLabeling,
+                .fluidOunceUSCustomary,
+                .fluidOunceImperial
+            ]
+        case .pint:
+            return [
+                .pintUSLiquid,
+                .pintMetric,
+                .pintImperial,
+                .pintFlemishPintje,
+                .pintIndia,
+                .pintSouthAustralia,
+                .pintAustralia,
+                .pintRoyal,
+                .pintCanada,
+            ]
+        case .quart:
+            return [
+                .quartUSLiquid,
+                .quartImperial,
+            ]
+        case .gallon:
+            return [
+                .gallonUSLiquid,
+                .gallonImperial,
+            ]
+        default:
+            return []
+        }
+    }
+}
