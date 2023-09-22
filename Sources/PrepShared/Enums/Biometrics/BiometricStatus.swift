db06e4d5c5316b9c73bfdadd87790c6226ba467a
Ahmed Khalaf <pxlshpr@gmail.com>
2023-09-22 11:26:41 +0500

    bump

--- Sources/PrepShared/Enums/Biometrics/BiometricStatus.swift
+++ Sources/PrepShared/Enums/Biometrics/BiometricStatus.swift
@@ -5,16 +5,3 @@ public enum BiometricStatus {
     case syncFailing
     case manual
 }
-
-public extension BiometricStatus {
-    func message(for type: BiometricType) -> String {
-        switch self {
-        case .synced:
-            "Your \(type.abbreviation) is synced with the Health app and will automatically re-calculate any dependent goals when it changes."
-        case .syncFailing:
-            "Make sure you have allowed Prep to read your \(type.abbreviation) data in Settings > Privacy & Security > Health > Prep."
-        case .manual:
-            "Your will have to manually keep your \(type.abbreviation) updated here."
-        }
-    }
-}
