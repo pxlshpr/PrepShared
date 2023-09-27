6bcd0ad8912433891ef61fb8e7d84ae260f461ed
Ahmed Khalaf <pxlshpr@gmail.com>
2023-09-27 15:04:30 +0500

    bump

---
+++ Tests/PrepSharedTests/Auto Goals/File.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import PrepShared
+
+final class AutoEnergyGoalTests: XCTestCase {
+    
+    func testAutoEnergyGoal() throws {
+        XCTAssertTrue(true, "Let's go")
+    }
+}
