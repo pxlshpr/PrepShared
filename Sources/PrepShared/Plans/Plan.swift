d51291a77e61c6f961424f667ac95c23bf659c98
Ahmed Khalaf <pxlshpr@gmail.com>
2023-10-06 17:17:11 +0500

    tag increment

--- Sources/PrepShared/Plans/Plan.swift
+++ Sources/PrepShared/Plans/Plan.swift
@@ -4,15 +4,18 @@ public struct Plan: Identifiable, Hashable, Codable {
     
     public var id: UUID
     public var name: String
+    public var isDisabled: Bool
     public var goals: [Goal] = []
     
     public init(
         id: UUID = UUID(),
         name: String,
+        isDisabled: Bool = false,
         goals: [Goal] = []
     ) {
         self.id = id
         self.name = name
+        self.isDisabled = isDisabled
         self.goals = goals
     }
 }
