19497a644f7db463013b71cecb98abe4a4f40811
Ahmed Khalaf <pxlshpr@gmail.com>
2023-10-25 19:40:07 +0500

    tag increment

--- Sources/PrepShared/Biometrics/Structs/Quantity.swift
+++ Sources/PrepShared/Biometrics/Structs/Quantity.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct Quantity: Hashable, Codable {
     public var value: Double
     public var date: Date?
+    
+    public init(value: Double, date: Date? = nil) {
+        self.value = value
+        self.date = date
+    }
 }
