d1d558fa3b04148a7b7f39057451c2b988293495
Ahmed Khalaf <pxlshpr@gmail.com>
2023-10-29 11:27:31 +0500

    tag increment

--- Sources/PrepShared/Structs/Settings.swift
+++ Sources/PrepShared/Structs/Settings.swift
@@ -1,36 +1,36 @@
-import Foundation
-
-public struct Settings: Codable, Hashable {
-    
-    public var energyUnit: EnergyUnit
-    public var bodyMassUnit: BodyMassUnit
-    public var heightUnit: HeightUnit
-    public var volumeUnits: VolumeUnits
-    
-    public var macrosBarType: MacrosBarType
-    
-    public var nutrientsFilter: NutrientsFilter
-    public var showRDAGoals: Bool
-    public var expandedMicroGroups: [MicroGroup]
-    public var metricType: GoalMetricType
-}
-
-public extension Settings {
-    static var `default`: Settings {
-        Settings(
-            energyUnit: .kcal,
-            bodyMassUnit: .kg,
-            heightUnit: .cm,
-            volumeUnits: .defaultUnits,
-            macrosBarType: .foodItem,
-            nutrientsFilter: .all,
-            showRDAGoals: true,
-            expandedMicroGroups: [],
-            metricType: .consumed
-        )
-    }
-    
-    var asData: Data {
-        try! JSONEncoder().encode(self)
-    }
-}
+//import Foundation
+//
+//public struct Settings: Codable, Hashable {
+//    
+//    public var energyUnit: EnergyUnit
+//    public var bodyMassUnit: BodyMassUnit
+//    public var heightUnit: HeightUnit
+//    public var volumeUnits: VolumeUnits
+//    
+//    public var macrosBarType: MacrosBarType
+//    
+//    public var nutrientsFilter: NutrientsFilter
+//    public var showRDAGoals: Bool
+//    public var expandedMicroGroups: [MicroGroup]
+//    public var metricType: GoalMetricType
+//}
+//
+//public extension Settings {
+//    static var `default`: Settings {
+//        Settings(
+//            energyUnit: .kcal,
+//            bodyMassUnit: .kg,
+//            heightUnit: .cm,
+//            volumeUnits: .defaultUnits,
+//            macrosBarType: .foodItem,
+//            nutrientsFilter: .all,
+//            showRDAGoals: true,
+//            expandedMicroGroups: [],
+//            metricType: .consumed
+//        )
+//    }
+//    
+//    var asData: Data {
+//        try! JSONEncoder().encode(self)
+//    }
+//}
