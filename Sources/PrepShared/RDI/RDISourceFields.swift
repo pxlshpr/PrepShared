62340995148d84c477772c69031b6a631582f803
Ahmed Khalaf <pxlshpr@gmail.com>
2023-10-31 20:49:38 +0500

    tag increment

--- Sources/PrepShared/RDI/RDISourceFields.swift
+++ Sources/PrepShared/RDI/RDISourceFields.swift
@@ -6,7 +6,7 @@ public struct RDISourceFields: Hashable, Equatable {
     public var detail: String
     public var url: String
     
-    init(
+    public init(
         abbreviation: String = "",
         name: String = "",
         detail: String = "",
