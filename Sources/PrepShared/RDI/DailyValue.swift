b3b5caae8afac7c3fe8e6a9b3c23941056530b7a
Ahmed Khalaf <pxlshpr@gmail.com>
2023-11-06 20:24:03 +0500

    tag increment

---
+++ Sources/PrepShared/RDI/DailyValue.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public struct DailyValue: Codable {
+    public let type: DailyValueType
+    public let rdi: RDI?
+    public let customValue: CustomValue?
+    
+    public struct CustomValue: Codable {
+        public let bound: Bound
+        public let unit: NutrientUnit
+        
+        public init(bound: Bound, unit: NutrientUnit) {
+            self.bound = bound
+            self.unit = unit
+        }
+    }
+    
+    public init(type: DailyValueType, rdi: RDI?, customValue: CustomValue?) {
+        self.type = type
+        self.rdi = rdi
+        self.customValue = customValue
+    }
+}
