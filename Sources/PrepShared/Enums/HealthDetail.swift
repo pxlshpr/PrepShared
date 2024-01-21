a06dcd2b49122b8ca079cae276f2a9121d2243ac
Ahmed Khalaf <pxlshpr@gmail.com>
2024-01-21 22:57:10 +0500

    tag increment

---
+++ Sources/PrepShared/Enums/HealthDetail.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+public enum HealthDetail: Int, Identifiable, CaseIterable, Codable {
+    case maintenance = 1
+    
+    case age
+    case biologicalSex
+    case weight
+    case leanBodyMass
+    case fatPercentage
+    case height
+    case preganancyStatus
+    case smokingStatus
+}
+
+public extension HealthDetail {
+    
+    var id: Int { rawValue }
+
+    var name: String {
+        switch self {
+        case .maintenance:  "Maintenance Energy"
+        case .age: "Age"
+        case .biologicalSex: "Biological Sex"
+        case .height: "Height"
+        case .weight: "Weight"
+        case .leanBodyMass: "Lean Body Mass"
+        case .fatPercentage: "Fat Percentage"
+        case .preganancyStatus: "Pregnancy Status"
+        case .smokingStatus: "Smoking Status"
+        }
+    }
+    
+    /// Not related to time—once set, the value is brought forward to every future HealthDetail until changed
+    var isNonTemporal: Bool {
+        switch self {
+        case .age, .biologicalSex, .smokingStatus:
+            true
+        default:
+            false
+        }
+    }
+    
+    /// Directly related to the date it's set on. When referenced, the date is displayed for these as they can potentially change on any future date.
+    var isTemporal: Bool {
+        !isNonTemporal
+    }
+    
+//    static var allNonTemporalHealthDetails: [HealthDetail] {
+//        allCases.filter{ $0.isNonTemporal }.sorted()
+//    }
+    
+    static var allTemporalHealthDetails: [HealthDetail] {
+        allCases.filter{ $0.isTemporal }.sorted()
+    }
+}
+
+extension HealthDetail: Comparable {
+    public static func < (lhs: HealthDetail, rhs: HealthDetail) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+}
+
+public extension Array where Element == HealthDetail {
+    var nonTemporalHealthDetails: [HealthDetail] {
+        filter{ $0.isNonTemporal }.sorted()
+    }
+    
+    var temporalHealthDetails: [HealthDetail] {
+        filter{ $0.isTemporal }.sorted()
+    }
+    
+    var containsAllCases: Bool {
+        HealthDetail.allCases.allSatisfy { contains($0) }
+    }
+    
+    var containsAllTemporalCases: Bool {
+        HealthDetail.allTemporalHealthDetails.allSatisfy { contains($0) }
+    }
+}
+
