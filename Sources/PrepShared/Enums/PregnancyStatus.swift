cb13ec0c49d049249476944bce8bf1f7e72e6cf3
Ahmed Khalaf <pxlshpr@gmail.com>
2024-01-22 00:43:26 +0500

    tag increment

--- Sources/PrepShared/Enums/PregnancyStatus.swift
+++ Sources/PrepShared/Enums/PregnancyStatus.swift
@@ -1,15 +1,17 @@
 public enum PregnancyStatus: Int16, Codable {
-    case notPregnantOrLactating
+    case notPregnantOrLactating = 1
     case pregnant
     case lactating
+    case notSet
 }
 
 public extension PregnancyStatus {
     var name: String {
         switch self {
-        case .notPregnantOrLactating:         "None"
-        case .pregnant:     "Pregnant"
-        case .lactating:    "Breastfeeding"
+        case .notPregnantOrLactating:   "None"
+        case .pregnant:                 "Pregnant"
+        case .lactating:                "Breastfeeding"
+        case .notSet:                   "Not Set"
         }
     }
 }
@@ -18,6 +20,6 @@ extension PregnancyStatus: Pickable {
     public var pickedTitle: String { name }
     public var menuTitle: String { name }
     public static var `default`: PregnancyStatus { .notPregnantOrLactating }
-    public static var noneOption: PregnancyStatus? { .notPregnantOrLactating }
+    public static var noneOption: PregnancyStatus? { .notSet }
 }
 
