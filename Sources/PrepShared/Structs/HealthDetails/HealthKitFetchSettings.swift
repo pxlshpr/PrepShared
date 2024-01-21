64a217f7dc9b9ad2c6db3350521b06d6e748e717
Ahmed Khalaf <pxlshpr@gmail.com>
2024-01-22 02:10:34 +0500

    tag increment

--- Sources/PrepShared/Structs/HealthDetails/HealthKitFetchSettings.swift
+++ Sources/PrepShared/Structs/HealthDetails/HealthKitFetchSettings.swift
@@ -1,12 +1,29 @@
 import Foundation
 
 public struct HealthKitFetchSettings: Hashable, Codable {
-    public var intervalType: HealthIntervalType = .average
-    public var interval: HealthInterval = .init(3, .day)
-    public var correction: Correction? = nil
+    public var intervalType: HealthIntervalType
+    public var interval: HealthInterval
+    public var correction: Correction?
 
+    public init(
+        intervalType: HealthIntervalType = .average,
+        interval: HealthInterval = .init(3, .day),
+        correction: Correction? = nil
+    ) {
+        self.intervalType = intervalType
+        self.interval = interval
+        self.correction = correction
+    }
     public struct Correction: Hashable, Codable {
         public let type: CorrectionType
         public let value: Double
+        
+        public init(
+            type: CorrectionType,
+            value: Double
+        ) {
+            self.type = type
+            self.value = value
+        }
     }
 }
